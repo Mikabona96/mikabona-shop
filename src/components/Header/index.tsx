5ef853af50e5089ebe0424b04e7fdfddff304429
Mika Bona <dmitriygolubapk@gmail.com>
2022-06-09 16:58:10 +0300

    registration page in progress

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,5 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
-import { signInWithGoogle } from '../../firebase'
-import { userState } from '../../recoil';
-
-
 export const Header = () => {
-  const [user, setUser] = useRecoilState(userState);
-  const navigate = useNavigate()
+
   return (
     <nav className='flex justify-between px-8 py-3 h-[10%] bg-slate-800 text-white'>
         <div className='flex justify-between w-20'>
@@ -18,9 +10,7 @@ export const Header = () => {
         <div className='flex justify-between w-40'>
             <p>Liked</p>
             <p>Cart</p>
-            <div onClick={() => {
-              navigate('/registration')
-            }}>Sign up</div>
+            <p>Sign up</p>
         </div>
     </nav>
   )
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,19 +1,54 @@
-import React from 'react'
 import GoogleIcon from '@mui/icons-material/Google';
+import { FcGoogle } from 'react-icons/fc';
+import { useRecoilState } from 'recoil';
+import { signInWithGoogle } from '../../firebase'
+import { userState } from '../../recoil';
 
 export const Registration = () => {
+    const [user, setUser] = useRecoilState(userState);
   return (
-    <div>
-        <form>
-            <label>
-                <input type="email" name='email' />
+    <div className='h-screen pl-56 flex justify-center my-[5rem]'>
+        <form className='w-[25rem]'>
+            <label className='text-2xl'>
+                Name:
+                <div className='border-2 border-black h-10 pl-2'>
+                    <input className='outline-0' type="text" name='name' />
+                </div>
             </label>
-            <label>
-                <input type="password" name='password' />
+            <label className='text-2xl'>
+                Email:
+                <div className='border-2 border-black h-10 pl-2'>
+                    <input className='outline-0' type="email" name='email' />
+                </div>
             </label>
+            <label className='text-2xl'>
+                Password:
+                <div className='border-2 border-black h-10 pl-2'>
+                    <input className='outline-0' type="password" name='password' />
+                </div>
+            </label>
+            <label className='text-2xl'>
+                Confirm Password:
+                <div className='border-2 border-black h-10 pl-2'>
+                    <input className='outline-0' type="password" name='password' />
+                </div>
+            </label>
+            <div className='border-2 border-black text-center text-2xl mt-5 hover:bg-black hover:text-white cursor-pointer h-10'>Sign up</div>
+            <div className='border-2 border-black text-center text-2xl mt-5 hover:bg-black hover:text-white cursor-pointer h-10 flex items-center content-center justify-center'
+            onClick={() => {
+                signInWithGoogle().then(res => {
+                    if (res.user) {
+                      setUser({
+                        displayName: (res.user.displayName as string),
+                        email: (res.user.email as string),
+                        photo: (res.user.photoURL as string),
+                      })
+                      localStorage.setItem('user', JSON.stringify(user))
+                    }
+                  })
+            }}>{'Sign up with '} <FcGoogle /></div>
         </form>
 
-        <div>Sign In with<GoogleIcon /></div>
     </div>
   )
 }
